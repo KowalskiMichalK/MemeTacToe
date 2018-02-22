c52259131efa942d765f5905bbb4ecafb5c1492a
Michał <Kowalski.Michal.K@gmail.com>
2018-02-22 21:48:59 +0100

    Project creation

---
+++ src/main/java/com/michal/memetactoe/MemeTacToeApplication.java
@@ -0,0 +1,12 @@
+package com.michal.memetactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeTacToeApplication.class, args);
+	}
+}
